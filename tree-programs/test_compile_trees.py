dfb9eb8e6fb30594868b8749cd817b4317f7339d
Ben North <ben@redfrontdoor.org>
2021-12-18 08:46:14 +0000

    replace_element(): Test

--- tree-programs/test_compile_trees.py
+++ tree-programs/test_compile_trees.py
@@ -12,3 +12,11 @@ class TestLeafNode:
         I = ct.InternalNode
         n = V(23)
         assert list(n.all_extended(11)) == [I([V(23), V(11)])]
+
+
+class TestHelpers:
+    def test_replace_element(self):
+        xs = [1, 5, 12, 10]
+        ys = ct.replace_element(xs, 1, 66)
+        assert xs == [1, 5, 12, 10]
+        assert ys == [1, 66, 12, 10]

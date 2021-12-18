e726f64572041426b700fccd93739fd228fe8a7c
Ben North <ben@redfrontdoor.org>
2021-12-18 08:45:05 +0000

    LeafNode.all_extended(): Test

--- tree-programs/test_compile_trees.py
+++ tree-programs/test_compile_trees.py
@@ -6,3 +6,9 @@ class TestLeafNode:
     def test_creation(self):
         n = ct.LeafNode(23)
         assert n.value == 23
+
+    def test_extension(self):
+        V = ct.LeafNode
+        I = ct.InternalNode
+        n = V(23)
+        assert list(n.all_extended(11)) == [I([V(23), V(11)])]

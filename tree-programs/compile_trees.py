4351558af211344e3d0df894f3dd4dc14feea871
Ben North <ben@redfrontdoor.org>
2021-12-18 08:37:20 +0000

    OpcodeKind: Add Enum

--- tree-programs/compile_trees.py
+++ tree-programs/compile_trees.py
@@ -1,6 +1,14 @@
 from dataclasses import dataclass
+from enum import Enum
 
 
 @dataclass
 class LeafNode:
     value: int
+
+
+class OpcodeKind(Enum):
+    Value = 0
+    MultiplyN = 1
+    AddN = 2
+    Return = 3

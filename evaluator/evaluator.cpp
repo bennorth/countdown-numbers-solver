1baf8dedb9a4bbbe8245815f1ea8f4e09267a2f8
Ben North <ben@redfrontdoor.org>
2021-12-19 09:15:00 +0000

    all_programs_: Add local array

--- evaluator/evaluator.cpp
+++ evaluator/evaluator.cpp
@@ -1,5 +1,9 @@
+#include <array>
 #include <cstddef>
 #include "programs-6-cards.h"
+#include "evaluator.h"
+
+static std::array<Opcode, programs_6_cards_bin_len * 2> all_programs_;
 
 const uint8_t * all_packed_programs()
 {

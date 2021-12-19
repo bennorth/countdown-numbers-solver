ce548186c76ad6b72d686de14d835ed2ba52568f
Ben North <ben@redfrontdoor.org>
2021-12-19 08:38:21 +0000

    Add "OpcodeKind manipulation" test

--- evaluator/test_evaluator.cpp
+++ evaluator/test_evaluator.cpp
@@ -2,3 +2,11 @@
 #include "catch.hpp"
 
 #include "evaluator.h"
+
+TEST_CASE("OpcodeKind manipulation", "")
+{
+  REQUIRE(other_binop(OpcodeKind::Value) == OpcodeKind::Value);
+  REQUIRE(other_binop(OpcodeKind::MultiplyN) == OpcodeKind::AddN);
+  REQUIRE(other_binop(OpcodeKind::AddN) == OpcodeKind::MultiplyN);
+  REQUIRE(other_binop(OpcodeKind::Return) == OpcodeKind::Return);
+}

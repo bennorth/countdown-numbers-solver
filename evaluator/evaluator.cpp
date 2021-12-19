886b55999aeac3fe3bdc0386cf00012885566084
Ben North <ben@redfrontdoor.org>
2021-12-19 09:15:12 +0000

    programs_6_cards_bin_end: Add constant

--- evaluator/evaluator.cpp
+++ evaluator/evaluator.cpp
@@ -3,6 +3,9 @@
 #include "programs-6-cards.h"
 #include "evaluator.h"
 
+static const uint8_t * programs_6_cards_bin_end
+  = programs_6_cards_bin + programs_6_cards_bin_len;
+
 static std::array<Opcode, programs_6_cards_bin_len * 2> all_programs_;
 
 const uint8_t * all_packed_programs()

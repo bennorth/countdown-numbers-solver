3002dce7aa0397b681e9aeafb55b4b4e8aae03c2
Ben North <ben@redfrontdoor.org>
2022-01-08 17:19:23 +0000

    ValueNode.from_string(): Test

--- compare-solutions/test_compare_solutions.py
+++ compare-solutions/test_compare_solutions.py
@@ -1,1 +1,9 @@
 import compare_solutions
+
+VN = compare_solutions.ValueNode
+
+
+class TestValueNode:
+    def test_from_string(self):
+        n = VN.from_string("V(123)")
+        assert n.value == 123

bc70fc27ca091fde5c3a08f001a4084e75faa00e
Ben North <ben@redfrontdoor.org>
2022-01-08 17:21:33 +0000

    tree_from_string(): Add

--- compare-solutions/compare_solutions.py
+++ compare-solutions/compare_solutions.py
@@ -29,3 +29,20 @@ class OpNode:
         operands = stack[-n_operands:]
         stack[-n_operands:] = []
         return cls(operands, ops)
+
+
+def tree_from_string(s):
+    opcodes = s.split()
+    ops = []
+    for c in opcodes:
+        k = c[0]
+        if k == "V":
+            ops.append(ValueNode.from_string(c))
+        elif k in "AM":
+            result = OpNode.from_string(c, ops)
+            ops.append(result)
+        elif k == "R":
+            # Should be last opcode.
+            pass
+    assert len(ops) == 1
+    return ops[0]

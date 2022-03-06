4cbe0b58446e8ded320024e15d58b8706d4e84b0
Ben North <ben@redfrontdoor.org>
2022-03-06 14:39:49 +0000

    Add wasm compilation command

--- evaluator/make-wasm.sh
+++ evaluator/make-wasm.sh
@@ -22,3 +22,12 @@ fi
 
 # shellcheck disable=SC1091
 source ./emsdk/emsdk_env.sh
+
+em++ \
+    --bind \
+    -s USE_BOOST_HEADERS=1 \
+    -O3 \
+    -o CountdownSolver.js \
+    solver-for-wasm.cpp \
+    evaluator.cpp \
+    programs-6-cards.cpp

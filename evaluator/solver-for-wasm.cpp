debd14220ba7578be1f37ac2e7a81274784cd861
Ben North <ben@redfrontdoor.org>
2022-03-06 14:39:08 +0000

    CountdownSolver: Add class (in own file)
    Ready for exposing via wasm.

---
+++ evaluator/solver-for-wasm.cpp
@@ -0,0 +1,40 @@
+#include <array>
+#include <vector>
+#include <stdint.h>
+#include "evaluator.h"
+
+struct CountdownSolver {
+  std::array<int, 6> cards;
+  int target;
+  std::vector<Opcode> solutions;
+
+  CountdownSolver(int c1, int c2, int c3, int c4, int c5, int c6, int target)
+    : cards{c1, c2, c3, c4, c5, c6}
+    , target(target)
+  {}
+
+  void solve() {
+    Evaluator::output_function_t process_candidate{
+      [this](const Evaluator & e) {
+        if (e.value() == target)
+          solutions.insert(solutions.end(),
+                           e.concrete_instructions.begin(),
+                           e.concrete_instructions.end());
+      }};
+
+    const Opcode * program{all_programs()};
+    Evaluator e(program, cards.data(), process_candidate);
+
+    bool more_follow = true;
+    while (more_follow)
+      more_follow = e.all_valid();
+  }
+
+  const intptr_t solution_programs() const {
+    return reinterpret_cast<intptr_t>(solutions.data());
+  }
+
+  const int solution_programs_nbytes() const {
+    return solutions.size() * sizeof(Opcode);
+  }
+};

6d487a6c2b2c2fd27f2ff2e5526e5711ce198c58
Ben North <ben@redfrontdoor.org>
2022-03-06 14:55:33 +0000

    pprintPrograms(): Add function

--- write-up/countdown-numbers.js
+++ write-up/countdown-numbers.js
@@ -15,3 +15,55 @@ const symbols = {
     "1": { nonInv: "×", inv: "÷" },
     "2": { nonInv: "+", inv: "−" },
 };
+
+const pprintPrograms = (opcode_u8s) => {
+    const cards = allCards();
+    let exprns = [];
+    let ops = [];
+    for (let u8Idx = 0; u8Idx !== opcode_u8s.length; u8Idx += 3) {
+        const opcode = opcode_u8s[u8Idx];
+        const arg0 = opcode_u8s[u8Idx + 1];
+        let arg1 = opcode_u8s[u8Idx + 2];
+
+        switch (opcode) {
+        case 0: // Value
+            ops.push(`${cards[arg0]}`);
+            break;
+        case 1: // Multiply } nArgs = arg0
+        case 2: // Add      } nonInvMask = arg1
+            {
+                const symInv = symbols[opcode].inv;
+                const symNonInv = symbols[opcode].nonInv;
+
+                let invInputs = [];
+                let nonInvInputs = [];
+                for (let operandIdx = 0; operandIdx != arg0; ++operandIdx, arg1 >>= 1) {
+                    const isNonInv = arg1 & 1;
+                    (isNonInv ? nonInvInputs : invInputs).push(ops.pop());
+                }
+                invInputs.sort((a, b) => (b - a));
+                nonInvInputs.sort((a, b) => (b - a));
+                let invSuffix;
+                switch (invInputs.length) {
+                case 0:
+                    invSuffix = "";
+                    break;
+                case 1:
+                    invSuffix = ` ${symInv} ${invInputs[0]}`;
+                    break;
+                default:
+                    invSuffix = ` ${symInv} (${invInputs.join(` ${symNonInv} `)})`;
+                    break;
+                }
+                ops.push(`(${nonInvInputs.join(` ${symNonInv} `)}${invSuffix})`);
+            }
+            break;
+        case 3: // Return
+            const fullExprn = ops.pop();
+            // Strip outermost ()s:
+            exprns.push(fullExprn.substr(1, fullExprn.length - 2));
+            break;
+        }
+    }
+    return exprns;
+};

3e3edab03d2af7367509ce81224e48e11347a49b
Ben North <ben@redfrontdoor.org>
2022-03-06 14:55:14 +0000

    JS: Add helper functions and look-up

--- write-up/countdown-numbers.js
+++ write-up/countdown-numbers.js
@@ -1,1 +1,17 @@
 // Take user inputs and feed to solver; display results.
+
+const inputValue = (id) => parseInt(document.getElementById(id).value, 10);
+const cardValue = (n) => inputValue(`c${n}`);
+
+const allCards = () => {
+    let values = [];
+    for (i = 0; i != 6; ++i) {
+        values.push(cardValue(i));
+    }
+    return values;
+}
+
+const symbols = {
+    "1": { nonInv: "×", inv: "÷" },
+    "2": { nonInv: "+", inv: "−" },
+};

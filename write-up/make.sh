4cebbb810030669cb3c1972f250b891c0cb739a9
Ben North <ben@redfrontdoor.org>
2022-03-06 14:54:38 +0000

    make.sh: Add script

---
+++ write-up/make.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+
+mkdir -p dist
+
+GITHUB_MARKDOWN_CSS=dist/github-markdown.css
+GITHUB_MARKDOWN_CSS_URL=https://raw.githubusercontent.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css
+
+if [ ! -f "${GITHUB_MARKDOWN_CSS}" ]; then
+    curl -o "${GITHUB_MARKDOWN_CSS}" "${GITHUB_MARKDOWN_CSS_URL}"
+fi
+
+EXTRA_SCRIPT_ARGS=()
+if [ "$1" = "dev" ]; then
+    EXTRA_SCRIPT_ARGS=(-A ./include-livereload.html)
+fi
+
+pandoc -s \
+       -f gfm \
+       --template template.html \
+       -o dist/index.html \
+       -c ./github-markdown.css \
+       -c ./countdown-numbers.css \
+       "${EXTRA_SCRIPT_ARGS[@]}" \
+       --metadata title="Finding solutions to the Countdown numbers round" \
+       index.md
+
+cp ../evaluator/CountdownSolver.js ../evaluator/CountdownSolver.wasm dist
+cp countdown-numbers.css countdown-numbers.js dist

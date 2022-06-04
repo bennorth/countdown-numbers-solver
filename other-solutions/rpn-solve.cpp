89ca739a629973b1a0420626a3651e331b2db265
Ben North <ben@redfrontdoor.org>
2022-06-04 23:18:21 +0100

    op_as_text(): Add

--- other-solutions/rpn-solve.cpp
+++ other-solutions/rpn-solve.cpp
@@ -5,3 +5,20 @@ enum class operation
     op_multiply,
     op_divide
 };
+
+const char *op_as_text(operation op)
+{
+    switch (op)
+    {
+    case operation::op_add:
+        return "A(++)";
+    case operation::op_subtract:
+        return "A(-+)";
+    case operation::op_multiply:
+        return "M(++)";
+    case operation::op_divide:
+        return "M(-+)";
+    default:
+        return "?????";
+    }
+}
